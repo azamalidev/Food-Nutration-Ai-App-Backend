9742fc2c28464522c5bdc204a0a479043b0985e9
zainab674 <zainabsarwar58@gmail.com>
2025-07-07 21:01:15 +0500

    v6

--- index.js
+++ index.js
@@ -2,14 +2,46 @@ import express from "express";
 import { } from "dotenv/config";
 import loaders from "./loaders/index.js";
 import config from "./config/index.js";
+import multer from "multer";
+// routes
 
-// Create app first
-const app = express();
-app.get("/favicon.ico", (req, res) => res.status(204).end()); // Avoid 500 on faviconn
+import userRoute from "./routes/user/index.js";
 
 
-// Initialize loaders (middleware, routes, etc.)
-await loaders.init({ expressApp: app });
+async function startServer() {
+	const app = express();
+	await loaders.init({ expressApp: app });
 
-// ✅ Vercel needs this export
-export default app;
+	const server = app.listen(config.env.port, () =>
+		console.log(`Server Started ~ :${config.env.port}`)
+	);
+
+	process.on("uncaughtException", (err) => {
+		console.log("uncaughtException! Shutting Down the Server...");
+		console.log(err);
+
+		process.exit(1);
+	});
+
+	process.on("unhandledRejection", (err) => {
+		console.log("unhandledRejection! Shutting Down the Server...");
+		console.log(err);
+		server.close(() => {
+			process.exit(1);
+		});
+	});
+}
+
+startServer();
+
+const protectedRouter = express.Router();
+const unProtectedRouter = express.Router();
+
+// Protected Routes
+// protectedRouter.use("/product", productRoute);
+
+
+// Un-Protected Routes
+unProtectedRouter.use("/", userRoute);
+
+export { protectedRouter, unProtectedRouter };
